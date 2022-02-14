4a3102795155dd380a980180177be142e5fbeac3
Manuel Casanova <manucasanova@hotmail.com>
2022-02-14 10:28:16 -0800

    refactor commit

--- index.js
+++ index.js
@@ -109,28 +109,39 @@ class Transaction {
       this.account = account;
     }
 
+    commit() {
+    this.account.balance += this.value;
+  }
 
 }
 
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
-  }
+  // commit() {
+  //   this.account.balance -= this.amount;
+  // }
+//Instead of having commit defined in each subclass, define a getter method called value in each subclass.
+get value() {
+  return -this.amount; //why not .value?
+}
+
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
-  }
-
+  // commit() {
+  //   this.account.balance += this.amount;
+  // }
+//Instead of having commit defined in each subclass, define a getter method called value in each subclass.
+get value() {
+  return this.amount; //Why not .value?
+}
 
 }
 
 class Account {
-  constructor(username, balance) {
+  constructor(username) {
     this.username = username;
     this.balance = 0;
   }
