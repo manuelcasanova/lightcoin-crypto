651c49fed4ca77c45635d0afc80444b753f3048f
Manuel Casanova <manucasanova@hotmail.com>
2022-02-14 10:04:39 -0800

    account class

--- index.js
+++ index.js
@@ -1,53 +1,101 @@
-let balance = 500.00;
+//SETUP AND DEPOSIT CLASS
 
+// let balance = 500.00;
+
+// class Withdrawal {
+
+//   constructor(amount) {
+//     this.amount = amount;
+//   }
+
+//   commit() {
+//     balance -= this.amount;
+//   }
+// }
+
+// class Deposit {
+
+//   constructor(amount) {
+//     this.amount = amount;
+//   }
+
+//   commit() {
+//     balance += this.amount;
+//   }
+
+
+// }
+
+
+
+
+// // DRIVER CODE BELOW
+// // We use the code below to "drive" the application logic above and make sure it's working as expected
+
+// t1 = new Withdrawal(50.25);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// console.log('Balance:', balance);
+
+// t3 = new Withdrawal(39.76);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// console.log('Balance:', balance);
+
+// t4 = new Deposit(100.00);
+// t4.commit();
+// console.log('Transaction 4:', t4);
+
+// console.log('Balance:', balance);
+
+
+//ACCOUNT CLASS
+
+
+class Account {
+  constructor(username, balance) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 class Withdrawal {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance -= this.amount;
   }
 }
 
 class Deposit {
 
-  constructor(amount) {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.balance += this.amount;
   }
 
 
 }
 
+//Driver code
 
+const myAccount = new Account("snow-patrol");
 
-
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
-
-t3 = new Withdrawal(39.76);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
-
-t4 = new Deposit(100.00);
-t4.commit();
-console.log('Transaction 4:', t4);
-
-console.log('Balance:', balance);
+console.log(`Transaction: ${t1.amount}`);
+console.log(`Balance: ${myAccount.balance}`);
